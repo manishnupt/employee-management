a7b2317b07a65d38ca90bb2993d8192883dbcc6f
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-09-04 08:48:48 -0500

    employee iam url fix

--- src/main/java/com/hrms/employee/management/utility/Constants.java
+++ src/main/java/com/hrms/employee/management/utility/Constants.java
@@ -3,5 +3,5 @@ package com.hrms.employee.management.utility;
 public class Constants {
     public static final Object ONBOARD_KEYCLOAK_USER = "/iamcontroller/onboard-kc-user";
     public static final String GET_ADMIN_TOKEN = "/iamcontroller/keycloak-token" ;
-    public static final String GRANT_ADMIN_ACCESS ="/iamcontrollrt/grant-super-admin-access" ;
+    public static final String GRANT_ADMIN_ACCESS ="/iamcontroller/grant-super-admin-access" ;
 }
